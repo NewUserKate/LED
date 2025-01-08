98731611cb351f0e0bba9b1a80c88dc2c127787d
NewUserKate <akazarovaa@gmail.com>
2025-01-08 11:06:25 +0200

    LED_Control is now a library

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,9 @@
 #include <Arduino.h>
 #include <Adafruit_PWMServoDriver.h>
+#include "LED_Control.h"
+
 Adafruit_PWMServoDriver pwm1 = Adafruit_PWMServoDriver(0x40);
 Adafruit_PWMServoDriver pwm2 = Adafruit_PWMServoDriver(0x41);
-
-class LED_Control{
-private:
-  Adafruit_PWMServoDriver* pwm;
-  int green;
-  int red;
-  int blue;
-public:
-  LED_Control(Adafruit_PWMServoDriver& indicatedpwm, int portGreen, int portRed, int portBlue){
-    pwm = &indicatedpwm;
-    green = portGreen;
-    red = portRed;
-    blue = portBlue;
-  }
-  
-  Adafruit_PWMServoDriver* getPWM(){
-    return pwm;
-  }
-  int getGreen(){
-    return green;
-  }
-  int getRed(){
-    return red;
-  }
-  int getBlue(){
-    return blue;
-  }
-  void SetColor(int brightness_of_green, int brightness_of_red, int brightness_of_blue){
-    (*pwm).setPin(green, brightness_of_green, true);
-    (*pwm).setPin(red, brightness_of_red, true);
-    (*pwm).setPin(blue, brightness_of_blue, true);
-  }
-};
 
 LED_Control LED_1(pwm1, 0, 1, 2);
 LED_Control LED_2(pwm1, 4, 5, 6);
@@ -47,6 +16,11 @@ LED_Control LED_7(pwm2, 8, 9, 10);
 
 LED_Control AllLEDs[7]={LED_1, LED_2, LED_3, LED_4, LED_5, LED_6, LED_7};
 
+void SetColorForAll(int brightness_of_green, int brightness_of_red, int brightness_of_blue){
+  for(int i = 0; i<7; i++){
+    AllLEDs[i].SetColor(brightness_of_green, brightness_of_red, brightness_of_blue);
+  }
+}
 
 void setup() {
   pwm1.begin();
@@ -58,6 +32,6 @@ void setup() {
   }
   
 void loop() {
-
+  
 }
 
